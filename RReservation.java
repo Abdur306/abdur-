ecd6dacdb45e76262383e9cbae16d041073203a8
Abdur306 <113154564+Abdur306@users.noreply.github.com>
2022-10-12 07:01:12 +0530

    Update RReservation.java

--- RReservation.java
+++ RReservation.java
@@ -1,4 +1,3 @@
-package Inc;
 
 import java.util.Scanner;
 
@@ -8,10 +7,13 @@ public class RReservation {
 	static int upperb = 2;
 	static int racb = 2;
 	static int w_listw = 2;
-	static String[] Name;
-	static int[] Age;
-	static String[] Birth;
-
+	static int count=1;
+	static int n1=0;
+	static String[] Name=new String[15];
+	static int[] Age=new int[15];
+	static String[] Birth=new String[15];
+    static int[] Seatno=new int[15];
+    static int[]Fare=new int[15];
 	public static void main(String[] args) {
 
 		Scanner s = new Scanner(System.in);
@@ -60,138 +62,108 @@ public class RReservation {
 
 	public static void Bookticket() {
 		Scanner s1 = new Scanner(System.in);
-		if (lowerb < 1 && middleb < 1 && upperb < 1 && racb < 1 && w_listw < 1) {
+		if (lowerb < 0 && middleb < 0 && upperb < 0 && racb < 0 && w_listw < 0) {
 			System.out.println("No Tickets are Available");
 			
 		}
 		else {
 		System.out.println("enter how many tickets");
-		int n1 = s1.nextInt();	
+		 n1 = s1.nextInt();	
 		if(n1<=10) {
-		System.out.println("enter the passenger name,age,birth_preference");
-		String newName;
-		int newAge;
-		String newBirth;
-		for (int i = 0; i < n1; ++i) {
+			for(int i=0;i<n1;i++)
+			{
+		System.out.println("enter the name");
+		Name[i]=s1.next();
+		System.out.println("enter the age");
+		int age=s1.nextInt();
+		Age[i]=age;
+		if(age>5)
+		{
+			Seatno[i]=count++;
+			Fare[i]=400;
+		}
+			else
+			{
+			Seatno[i]=0;
+			Fare[i]=200;
+			}
 			
-			newName = s1.next();
-			newAge = s1.nextInt();
-			newBirth = s1.next();
-			Name = update_Name(Name, newName);
-			Age = update_Age(Age, newAge);
+		System.out.println("enter berth");
+		Birth[i]=s1.next();
 			
-			if(newAge>=50)
-				newBirth="L";
-			//if(newAge<=10)
-				//newBirth="";
-
-				if (newBirth.equals("L")) {
-					if (lowerb > 0) {
-						Birth = update_Birth(Birth, newBirth);
-						lowerb--;
-					} else if (middleb > 0) {
-						Birth = update_Birth(Birth, "middle");
-						middleb--;
-					} else if (upperb > 0) {
-						Birth = update_Birth(Birth, "upper");
-						upperb--;
-					} else if (racb > 0) {
-						Birth = update_Birth(Birth, "rac");
-						racb--;
-					} else if (w_listw > 0) {
-						Birth = update_Birth(Birth, "w/l");
-						w_listw--;
-					}
-				}
-			if (newBirth.equals("M")) {
-				if (middleb > 0) {
-					Birth = update_Birth(Birth, newBirth);
+			if(age>=60)
+				Birth[i]="Lower";
+			if (Birth[i].equals("Lower")) {
+				if (lowerb > 0) {
+					Birth[i]="Lower"; 
+					lowerb--;
+				} else if (middleb > 0) {
+					Birth[i]="Middle";
 					middleb--;
 				} else if (upperb > 0) {
-					Birth = update_Birth(Birth, "upper");
+					Birth[i] =  "upper";
 					upperb--;
-				}else if (lowerb > 0) {
-					Birth = update_Birth(Birth, "lower");
-					lowerb--;
 				} else if (racb > 0) {
-					Birth = update_Birth(Birth, "racc");
+					Birth[i] = "rac";
 					racb--;
 				} else if (w_listw > 0) {
-					Birth = update_Birth(Birth, "wlc");
+					Birth[i]= "w/l";
 					w_listw--;
 				}
 			}
-			if (newBirth.equals("U")) {
-				if (upperb > 0) {
-					Birth = update_Birth(Birth, newBirth);
-					upperb--;
-				} else if (middleb > 0) {
-					Birth = update_Birth(Birth, "middle");
-					middleb--;
-				} else if (lowerb > 0) {
-					Birth = update_Birth(Birth, "lower");
-					lowerb--;
-				} else if (racb > 0) {
-					Birth = update_Birth(Birth, "rac");
-					racb--;
-				} else if (w_listw > 0) {
-					Birth = update_Birth(Birth, "w/l");
-					w_listw--;
-				}
-				
+		if (Birth[i].equals("Middle")) {
+			if (middleb > 0) {
+				Birth[i] ="Middle";
+				middleb--;
+			} else if (upperb > 0) {
+				Birth[i] =  "upper";
+				upperb--;
+			}else if (lowerb > 0) {
+				Birth[i] =  "lower";
+				lowerb--;
+			} else if (racb > 0) {
+				Birth[i] =  "racc";
+				racb--;
+			} else if (w_listw > 0) {
+				Birth[i] = "wlc";
+				w_listw--;
 			}
-		
 		}
-		System.out.println("Booked Successfully");
-		RReservation.view();
-		System.out.println( "Name\t" + "Age\t" +"seat\t"  +"Birth");
-		for (int k = 1; k <= Name.length; k++) {
-			System.out.println( Name[k - 1] + "\t" + Age[k - 1] +"\t"+    k + "\t"  +         Birth[k - 1]);
+		if (Birth[i].equals("Upper")) {
+			if (upperb > 0) {
+				Birth[i] =  "Upper";
+				upperb--;
+			} else if (middleb > 0) {
+				Birth[i] =  "Middle";
+				middleb--;
+			} else if (lowerb > 0) {
+				Birth[i] =  "lower";
+				lowerb--;
+			} else if (racb > 0) {
+				Birth[i] = "rac";
+				racb--;
+			} else if (w_listw > 0) {
+				Birth[i] =  "w/l";
+				w_listw--;
+			}
+			
 		}
-
-	}
-		else
-			System.out.println("currently 10 tickets are available .plz enter below 10");
-	}
-		
-	}
 	
-	private static String[] update_Birth(String[] existingBerth, String string) {
-		if (existingBerth == null) {
-			existingBerth = new String[0];
+	}
 		}
-		String[] berth = new String[existingBerth.length + 1];
-		for (int i = 0; i < existingBerth.length; i++) {
-			berth[i] = existingBerth[i];
+	System.out.println("Booked Successfully");
+			}
+		
+		RReservation.view();
+		System.out.println( "Name\t" + "Age\t" +"seat\t"  +"Birth\t"+ "Fare\t");
+		for (int k = 0; k < Fare.length; k++) {
+			if(Fare[k]!=0)
+			System.out.println( Name[k] + "\t" + Age[k] +"\t"+    Seatno[k] + "\t"  +     Birth[k]+" \t" +Fare[k]+"\t");
 		}
-		berth[existingBerth.length] = string;
-		return berth;
+
 	}
 
-	private static int[] update_Age(int[] existingAge, int number) {
-		if (existingAge == null) {
-			existingAge = new int[0];
-		}
-		int[] age = new int[existingAge.length + 1];
-		for (int i = 0; i < existingAge.length; i++) {
-			age[i] = existingAge[i];
-		}
-		age[existingAge.length] = number;
-		return age;
-	}
-
-	private static String[] update_Name(String[] existingName, String string) {
-		if (existingName == null) {
-			existingName = new String[0];
-		}
-		String[] name = new String[existingName.length + 1];
-		for (int i = 0; i < existingName.length; i++) {
-			name[i] = existingName[i];
-		}
-		name[existingName.length] = string;
-		return name;
-	}
-	// }
+	
 
 public static void view() {
 	System.out.println(" ----------------------\t"+  "        ------------------------\t"+  " -------------------------\t"+  " ----------------\t");
