fe7919618cccfb0b969ffbc16d12e9644d86795c
Abdur306 <113154564+Abdur306@users.noreply.github.com>
2023-01-05 07:03:09 +0530

    Create BusBooking2.java

--- BusBooking2.java
+++ BusBooking2.java
@@ -1,5 +1,10 @@
-import java.util.*;
-class BusBooking2{
+package Practice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BusBoooking {
+
 public static void main(String [] abu){
 
 Reservation res=new Reservation();
@@ -15,7 +20,7 @@ static Scanner s2=new Scanner(System.in);
 	  String Sleeper[][]=new String[3][2];
 	   int seat=6;
 		 int col=2;
-	    int sleep=4;
+	    int sleep=3;
 	 int tseat=seat*col;
 	 int tsleep=sleep*col;
 	 int ks=0;
@@ -23,7 +28,7 @@ static Scanner s2=new Scanner(System.in);
 	ArrayList<Details>arr=new ArrayList<>();
 	
 	public void MainReserve(){
-	System.out.println("1.Availabilities || 2.Book || 3.Cancel || 4.view");	
+	System.out.println("1.Starting || 2.Book || 3.Cancel || 4.Availabilites || 5.Show Tickets");	
 		int input=s2.nextInt();
 		switch(input){
 			
@@ -39,14 +44,16 @@ static Scanner s2=new Scanner(System.in);
 			case 4:
 			    Display1();
 				break;
+			case 5:
+				ShowTicks();
+				break;
 			
 		}
 	}
 	public void Available(){
-		Admin ad=new Admin();
 		int inc=1;
-		System.out.println("seat:"+seat);
-			System.out.println("sleep:"+sleep);
+		System.out.println("seat:"+tseat);
+			System.out.println("sleep:"+tsleep);
 		System.out.println("Available Seater Seats");
 		for(int i=0;i<6;i++){
 			for(int j=0;j<2;j++){
@@ -58,8 +65,8 @@ static Scanner s2=new Scanner(System.in);
 		System.out.println("Available Sleepers berth");
 		for(int i=0;i<3;i++){
 			for(int j=0;j<2;j++){
-				Sleeper[i][j]=String.valueOf(inc++);	
-     System.out.print(Sleeper[i][j]+"U ");				
+				Sleeper[i][j]=String.valueOf(inc++)+"U";	
+     System.out.print(Sleeper[i][j]+" ");				
 			}
 			System.out.println();
 		}
@@ -107,7 +114,7 @@ static Scanner s2=new Scanner(System.in);
               System.out.println("Re-Enter valid Seat No");
 			  Book();
 				}
-				System.out.println(deepToString(Seater));
+	//			System.out.println(deepToString(Seater));
            MainReserve();				
 			}
 			else{
@@ -115,33 +122,43 @@ static Scanner s2=new Scanner(System.in);
 			}
 			
 		}
-		else if(bok==2){
-			//int tsleep=sleep*col;
+		if(bok==2){
+			//int tseat=seat*col;
 			if(tsleep>0){
 				System.out.println("Available:"+tsleep);
-				System.out.println("Enter Sleeper Seat No");
-				String sleep_sno=s2.next();
-				for(int i=0;i<6;i++){
+				System.out.println("Enter Seat No");
+				String seat_sno=s2.next();
+				boolean result=true;
+				for(int i=0;i<3;i++){
 				for(int j=0;j<2;j++){
-				if(sleep_sno.equals(Sleeper[i][j])){
-				System.out.println("Sleeper Ticket Booked success");
-                 Seater[i][j]=" ";
-				 arr.add(kslp++,new Details(cname,cage,cgender,cnum,cmail,cpickup,cdrop,sleep_sno));
+				//	System.out.println(Seater[i][j]);
+				if(String.valueOf(seat_sno).equals(Sleeper[i][j])){
+				System.out.println("Seater Ticket Booked success");
+                 Sleeper[i][j]=" ";
+				 arr.add(kslp++,new Details(cname,cage,cgender,cnum,cmail,cpickup,cdrop,seat_sno));
+				 
+			    result=false;
 				 tsleep--;
 				 break;
-				}else{
+				}
+				}
+				}
+			  if(result){
               System.out.println("Re-Enter valid Seat No");
 			  Book();
 				}
-				}
-				}
-				System.out.println(deepToString(Sleeper));
+	//			System.out.println(deepToString(Seater));
            MainReserve();				
 			}
 			else{
 				System.out.println("Currently no tickets ");
 			}
+			
 		}
+	}
+	private void ShowTicks() {
+		
+		
 	}
 	public  void Display1(){
 		System.out.println("Seater Seats");
@@ -163,8 +180,34 @@ static Scanner s2=new Scanner(System.in);
 		MainReserve();
 	}
 	public void  Cancel(){
-		
-		
+		System.out.println("1.Seater || 2.Sleeper");
+		int cin=s2.nextInt();
+		if(cin==1) {
+		System.out.println("Enter the SeatNo to Cancel");
+		String cseat_no=s2.next();
+		for(int i=0;i<6;i++){
+			for(int j=0;j<2;j++){
+				if(String.valueOf(cseat_no).equals(Seater[i][j])){
+					System.out.println("Seater Ticket Cancelled success");
+	                 Seater[i][j]=cseat_no;
+				}
+			}
+				System.out.println();
+		}MainReserve();
+		}
+		if(cin==2) {
+			System.out.println("Enter the SleeperNo to Cancel");
+			String cseat_no=s2.next();
+			for(int i=0;i<3;i++){
+				for(int j=0;j<2;j++){
+					if(String.valueOf(cseat_no).equals(Sleeper[i][j])){
+						System.out.println("Sleeper Ticket Cancelled success");
+		                 Sleeper[i][j]=cseat_no;
+					}
+				}
+	     		System.out.println();
+			}	
+		}MainReserve();
 	}
 }
 class Details{
@@ -190,3 +233,4 @@ public String toString(){
 	return name+" "+age+" "+gender+" "+phone+" "+mailid+" "+pickup+" "+drop+" "+seatno;
 }
 }
+
